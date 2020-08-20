95b69dac7ca17fb05f9ccd12e62a318a6b24b46a
marmorear <52580594+marmorear@users.noreply.github.com>
2020-08-20 17:46:43 -0300

    Update skelearn_transforms.py

--- my_custom_sklearn_transforms/skelearn_transforms.py
+++ my_custom_sklearn_transforms/skelearn_transforms.py
@@ -1,20 +1,49 @@
-
-
-from sklearn.base import BaseEstimator, TransformerMixin
+  
+de  sklearn . importação de base  BaseEstimator , TransformerMixin 
+importar  imblearn
+de  imblearn . over_sampling  import  SMOTE
+importar  pandas  como  pd
 from sklearn.ensemble import GradientBoostingClassifier
 
 dtc_model = GradientBoostingClassifier() 
 
-# All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
 
-    def fit(self, X, y=None):
-        return self
+# Todas as transformações sklearn devem ter os métodos `transform` e` fit`
+classe  DropColumns ( BaseEstimator , TransformerMixin ):
+    def  __init__ ( self , colunas ):
+        eu . colunas  =  colunas
 
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
+    def  fit ( self , X , y = None ):
+        retornar a  si mesmo
+
+    def  transform ( self , X ):
+        # Primeiro realiza uma cópia do dataframe 'X' de entrada
+        Dados  =  X . cópia ()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+         dados de retorno . descartar ( rótulos = auto . colunas , eixo = 'colunas' )
+    
+classe  SetIndex ( BaseEstimator , TransformerMixin ):
+    def  __init__ ( self , colunas ):
+        eu . colunas  =  colunas
+
+    def  fit ( self , X , y = None ):
+        retornar a  si mesmo
+
+    def  transform ( self , X ):
+        # Primeiro realiza uma cópia do dataframe 'X' de entrada
+        Dados  =  X . cópia ()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+         dados de retorno . set_index ( auto . colunas , inplace = Verdadeiro )
+    
+   
+classe  SmoteResample ( objeto ):
+    def  __init__ ( self ):
+        passar
+
+    def  fit ( self , X , y ):
+        X_resampled , y_resampled  =  SMOTE (). fit_resample ( X , y )
+        X_resampled  =  pd . Trama de dados ( X_resampled , colunas = X . Colunas )
+        return  X_resampled , y_resampled
+
+
+
